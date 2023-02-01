ee369098f2ba19e1cc22142166752eace1293e43
Andrea Childress <aesselman0830@gmail.com>
2023-02-01 15:37:21 -0500

    Adds camera button functionality + styling

--- CamApp2/screens/Camera.tsx
+++ CamApp2/screens/Camera.tsx
@@ -145,6 +145,8 @@ export default class Camera extends React.Component<CameraParamList> {
     if (this.camera) {
       const data = await this.camera.takePictureAsync();
       console.warn('takePicture ', data);
+    } else {
+      console.warn('hmm that didnt work');
     }
   };
 
@@ -176,13 +178,36 @@ export default class Camera extends React.Component<CameraParamList> {
         />
         <View>
           <View style={styles.camButton}>
-            <TouchableOpacity style={styles.capture}>
+            <TouchableOpacity
+              style={styles.capture}
+              onPress={this.takePicture.bind(this)}>
               <Text>SNAP</Text>
             </TouchableOpacity>
           </View>
+          <View style={styles.buttonView}>
+            <View style={styles.buttonRow}>
+              <TouchableOpacity
+                style={styles.flipButton}
+                onPress={this.toggleFacing.bind(this)}>
+                <Text style={styles.flipText}> FLIP </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.flipButton}
+                onPress={this.toggleFlash.bind(this)}>
+                <Text style={styles.flipText}> FLASH: {this.state.flash} </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.flipButton}
+                onPress={this.toggleWB.bind(this)}>
+                <Text style={styles.flipText}>
+                  {' '}
+                  WB: {this.state.whiteBalance}{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
       </>
-      //   </RNCamera>
     );
   }
 
@@ -210,5 +235,34 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     alignSelf: 'center',
     margin: 20,
+  },
+  buttonView: {
+    flex: 0,
+    height: 72,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  buttonRow: {
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  flipButton: {
+    flex: 0,
+    height: 40,
+    marginHorizontal: 2,
+    marginBottom: 10,
+    marginTop: 10,
+    borderRadius: 8,
+    borderColor: 'red',
+    borderWidth: 1,
+    padding: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  flipText: {
+    color: 'white',
+    fontSize: 15,
   },
 });
