af9359435b8988ca044e916cb77672f015fa89eb
Andrea Childress <aesselman0830@gmail.com>
2023-02-02 15:38:44 -0500

    Adds Stripe wrapper to app

--- CamApp2/App.tsx
+++ CamApp2/App.tsx
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -13,6 +6,8 @@ import Signup from './screens/Signup';
 import Login from './screens/Login';
 import PrintPhotos from './screens/PrintPhotos';
 import Camera from './screens/Camera';
+import {StripeProvider} from '@stripe/stripe-react-native';
+import {STRIPE_PUBLISHABLE_KEY} from '@env';
 
 type StackParamList = {
   Home: undefined;
@@ -26,16 +21,20 @@ const Stack = createStackNavigator<StackParamList>();
 
 export default function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{headerShown: false}}
-        initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Print" component={PrintPhotos} />
-        <Stack.Screen name="Camera" component={Camera} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <StripeProvider
+      publishableKey="pk_test_51LfaIxG2BxuQDWVz85mtNGNT0ulHZfGEnrGrUMtS4RNemK07IYCXgQl1Vj9BCUBjrfYkZXdeC4Lnq65zOsh5M9CL00YJsO7Ymt"
+      merchantIdentifier="merchant.com.camapp">
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{headerShown: false}}
+          initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Print" component={PrintPhotos} />
+          <Stack.Screen name="Camera" component={Camera} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </StripeProvider>
   );
 }
